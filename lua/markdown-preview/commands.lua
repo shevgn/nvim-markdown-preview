225ab32c4baacdb4c41ca3c99cc71ace22ba93f2
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-03-04 21:02:47 +0200

    commands naming change

--- lua/markdown-preview/commands.lua
+++ lua/markdown-preview/commands.lua
@@ -1,9 +1,9 @@
 local md = require("markdown-preview")
 
-vim.api.nvim_create_user_command("PreviewMarkdown", function()
+vim.api.nvim_create_user_command("MarkdownPreview", function()
 	md.start_preview()
 end, {})
 
-vim.api.nvim_create_user_command("PreviewMarkdownStop", function()
+vim.api.nvim_create_user_command("MarkdownPreviewStop", function()
 	md.stop_preview()
 end, {})
